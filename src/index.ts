bb9a9ce8a5f9590a2d147cefca9d94bbc579f11b
leapvn-anhdtt <anhdtt@leap.vn>
2024-10-27 01:16:26 +0700

    add put, delete endpoint

--- src/index.ts
+++ src/index.ts
@@ -37,6 +37,46 @@ app.post("/notes", async (req: Request, res: Response) => {
   }
 });
 
+app.put("/api/notes/:id", async (req: any, res: any) => {
+  const { title, content } = req.body;
+  const id = parseInt(req.params.id);
+
+  if (!title || !content) {
+    return res.status(400).send("Title and content fields are required.");
+  }
+
+  if (isNaN(id)) {
+    return res.status(400).send("ID must be a valid number.");
+  }
+
+  try {
+    const updatedNote = await prisma.note.update({
+      where: { id },
+      data: { title, content },
+    });
+    res.json(updatedNote);
+  } catch (error) {
+    res.status(500).send("An error occurred while updating the note.");
+  }
+});
+
+app.delete("/api/notes/:id", async (req: any, res: any) => {
+  const id = parseInt(req.params.id);
+
+  if (!id || isNaN(id)) {
+    return res.status(400).send("ID field required");
+  }
+
+  try {
+    await prisma.note.delete({
+      where: { id },
+    });
+    res.status(204).send();
+  } catch (error) {
+    res.status(500).send("Oops, something went wrong");
+  }
+});
+
 app.listen(5500, () => {
   console.log("server running on localhost:5500");
 });
